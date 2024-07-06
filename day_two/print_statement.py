6950bbe433d21cae09bb5d393084051da72a755b
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-07-06 10:37:23 +0500

    2.1/100

--- day_two/print_statement.py
+++ day_two/print_statement.py
@@ -6,3 +6,5 @@ print('Zulfiqar Alam')
 print("""Zulfiqar Alam""")
 
 print("'Zulfiqar Alam'") # This one is different
+
+print("Zulfiqar",4,8.8)
---
+++ day_two/sep_end_operators.py
@@ -0,0 +1,6 @@
+
+print("Zulfiqar Alam",4,5,8, sep="~")   #standard
+
+print("Zulfiqar",4,8,"Alam", end="009\n") 
+
+print("Zulfiqar ",3,4.5, sep="~") 
