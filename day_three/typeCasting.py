ff41a81a94fda40976d475c704dea6ffe1256414
Zulfiqar Alam <zulfi18.official@gmail.com>
2024-07-07 14:31:47 +0500

    3.2/100

---
+++ day_three/typeCasting.py
@@ -0,0 +1,32 @@
+
+a = 4.5
+print(type(a))
+
+b = int(a)
+
+print(type(b))
+
+#...................
+
+a = "10"
+print(type(a))
+
+b = int(a)
+
+print(type(b))
+
+#...................
+
+a = "10.5"
+print(type(a))
+
+b = float(a)
+
+print(type(b))
+
+#...................
+
+a = "True"
+print(type(a))
+
+b = bool(a)
